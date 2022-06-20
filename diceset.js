aef82281ccf6f891b3e3ef0cc16feea0760b5172
Tural <thetural@gmail.com>
2022-06-20 09:58:56 -0500

    Update diceset.js
    Faces were backwards in the code

--- diceset.js
+++ diceset.js
@@ -3,10 +3,10 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addDicePreset({
 		type:"dc",
 		labels:[
-			'modules/tymora-coin/textures/heads.webp','modules/tymora-coin/textures/tails.webp'
+			'modules/tymora-coin/textures/tails.webp','modules/tymora-coin/textures/heads.webp'
 		],
 		bumpMaps:[
-			'modules/tymora-coin/textures/heads_bump.webp','modules/tymora-coin/textures/tails_bump.webp'
+			'modules/tymora-coin/textures/tails_bump.webp','modules/tymora-coin/textures/heads_bump.webp'
 		],
 		system:"tymora-coin"
 	});
